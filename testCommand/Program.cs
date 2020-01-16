f4c27c9b1622b0f8374f9deda00977622a4c88c8
cash <cash@cashwu.com>
2020-01-16 15:45:00 +0800

    :tada: Initial commit.

---
+++ testCommand/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using McMaster.Extensions.CommandLineUtils;
+
+namespace testCommand
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            return CommandLineApplication.Execute<Program>(args);
+        }
+
+        [Option(Description = "subject")]
+        public string Subject { get; set; }
+        
+        [Option(Description = "nn")]
+        public int Count { get; set; }
+
+        private void OnExecute()
+        {
+            var name = Prompt.GetString("what's ur name ?");
+
+            Console.WriteLine($"==== ur name is {name} =====");
+            
+            var subject = Subject ?? "Hello World";
+
+            Console.WriteLine(subject);
+            Console.WriteLine(Count);
+        }
+        
+        // dotnet testCommand.dll -help
+        // dotnet testCommand.dll -s cash -c 123
+    }
+}
